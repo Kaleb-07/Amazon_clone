34a86fa504b5a32cab382600b6b28678143dda0c
Kaleb <kalebmulugeta07@gmail.com>
2026-01-02 08:13:24 -0600

    updating the main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { DataProvider } from './components/DataProvider/DataProvider.jsx'
-import { reducer, initialState } from './Utility/REDUCER.JS'
+import { reducer, initialState } from './Utility/reducer.JS'
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
